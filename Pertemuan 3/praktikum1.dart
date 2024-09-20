1c88d58c2b8b1d07f142916c7ebbe29fae362c39
gwenziro <144767417+gwenziro@users.noreply.github.com>
2024-09-20 09:26:16 +0700

    Create praktikum1.dart
    Tugas Praktikum 1

---
+++ Pertemuan 3/praktikum1.dart
@@ -0,0 +1,22 @@
+void main() {
+  String test = "test2";
+
+  if (test == "test1") {
+    print("Test1");
+  } else if (test == "test2") {
+    print("Test2");
+  } else {
+    print("Something else");
+  }
+
+  if (test == "test2") print("Test2 again");
+  // Eror
+  // String test = "true";
+
+  // Perbaikan
+  String test = true;
+
+  if (test) {
+    print("Kebenaran");
+  }
+}

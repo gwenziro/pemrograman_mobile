50a6eec8a5817e5b7f7e8071a2fbb71224b9dae2
gwenziro <144767417+gwenziro@users.noreply.github.com>
2024-10-04 08:25:09 +0700

    Tugas dan Praktikum

--- Pertemuan 4/src/praktikum5.dart
+++ Pertemuan 4/src/praktikum5.dart
@@ -2,8 +2,27 @@ void main() {
   // Langkah 1
   var record = ('first', a: 2, b: true, 'last');
   print(record);
+
+  // Langkah 4
+  // Record type annotation in a variable declaration:
+  // (String, int) mahasiswa;
+  // print(mahasiswa);
+
+  // Perbaikan Langkah 4
+  (String, int) mahasiswa;
+  mahasiswa = ('Muhamamd Rayhan', 2241720213);
+  print(mahasiswa);
+
+  // Langkah 5
+  var mahasiswa2 = ('first', a: 2, b: true, 'last');
+
+  print(mahasiswa2.$1); // Prints 'first'
+  print(mahasiswa2.a); // Prints 2
+  print(mahasiswa2.b); // Prints true
+  print(mahasiswa2.$2); // Prints 'last's
 }
 
+// Langkah 3
 (int, int) tukar((int, int) record) {
   var (a, b) = record;
   return (b, a);
